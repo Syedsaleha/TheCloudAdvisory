0a618c2a8c30b9f1978e6ba1c779945d20204ef2
Syed Saleha <117304081+Syedsaleha@users.noreply.github.com>
2025-11-17 12:54:21 +0530

    Updated Courses.jsx

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,12 +1,12 @@
 export default function Courses() {
   const courses = [
     {
-      title: "AWS Cloud Mastery",
-      duration: "6 Weeks",
-      price: "₹4,999",
+      title: "AWS DevOps & CI/CD with AWS CodePipeline for Engineers",
+      duration: "15 hours",
+      price: "₹659",
       status: "Live",
       img: "https://imgproxy.learnyst.com/learnyst-user-assets/school-assets/schools/165134/courses/193967/1718612700859Course_Thumbnail__1__lyst1718612700913.png?w=805&h=453&resizetype=fill",
-      link: "https://www.udemy.com/course/aws-cloud-master-course/?couponCode=YOUR_COUPON",
+      link: "https://www.udemy.com/course/mastering-aws-devops-for-cloud-engineers/?couponCode=PMNVD2025",
     },
     {
       title: "Docker & Kubernetes Bootcamp",
