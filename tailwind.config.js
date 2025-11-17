e70f2930468eaae3bd938215193f5c1d888f07ff
Syed Saleha <117304081+Syedsaleha@users.noreply.github.com>
2025-11-17 15:15:36 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-  keyframes: {
-    fadeInUp: {
-      "0%": { opacity: "0", transform: "translateY(40px)" },
-      "100%": { opacity: "1", transform: "translateY(0)" },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-out forwards",
+      },
     },
-    fadeIn: {
-      "0%": { opacity: "0" },
-      "100%": { opacity: "1" },
-    },
-  },
-  animation: {
-    fadeInUp: "fadeInUp 0.8s ease-out forwards",
-    fadeIn: "fadeIn 1s ease-out forwards",
-  },
-}
-
   },
   plugins: [],
 };
