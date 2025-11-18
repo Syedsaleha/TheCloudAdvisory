555c7797e40706b4b7be2e5b062c94c8ed1a694c
Syed Saleha <117304081+Syedsaleha@users.noreply.github.com>
2025-11-18 06:59:48 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// Initialize AOS
+AOS.init();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
