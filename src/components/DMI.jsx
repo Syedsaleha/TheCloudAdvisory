fd73876a3670345d08ab532bed7d8ab63a45f657
Syed Saleha <117304081+Syedsaleha@users.noreply.github.com>
2025-11-18 07:55:26 +0530

    Updated DMI.jsx

--- src/components/DMI.jsx
+++ src/components/DMI.jsx
@@ -1,18 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  Navigation as SwiperNavigation,
-  Pagination,
-  Autoplay,
-} from "swiper/modules";
-
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
-import Navigation from "./Navigation"; // your navbar
-import Footer from "./Footer";
-import { useEffect } from "react";
-
 
 
 export default function DMI() {
