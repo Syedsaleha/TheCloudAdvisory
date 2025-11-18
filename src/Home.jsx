566178af42f349a1a5f890da9c5353c6b5eab6fd
Syed Saleha <117304081+Syedsaleha@users.noreply.github.com>
2025-11-18 07:04:26 +0530

    Updated Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,37 +11,39 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white overflow-x-hidden">
 
-      {/* Navigation */}
       <Navigation />
 
-      {/* Sections */}
-      <section id="home">
+      {/* HERO */}
+      <section id="home" data-aos="fade-up" data-aos-duration="1000">
         <Hero />
       </section>
 
-      <section id="services">
+      {/* SERVICES */}
+      <section id="services" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
         <Services />
       </section>
 
-      <section id="book">
+      {/* BOOKS */}
+      <section id="book" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
         <Books />
       </section>
 
-      <section id="courses">
+      {/* COURSES */}
+      <section id="courses" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
         <Courses />
       </section>
 
-      <section id="about">
+      {/* ABOUT */}
+      <section id="about" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
         <About />
       </section>
 
-      <section id="contact">
+      {/* CONTACT */}
+      <section id="contact" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
         <Contact />
       </section>
 
-      {/* Footer */}
       <Footer />
-
     </div>
   );
 }
