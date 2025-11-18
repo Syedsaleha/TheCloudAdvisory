adcc14d6e15da66c6e33a43498697f34028e8b74
Syed Saleha <117304081+Syedsaleha@users.noreply.github.com>
2025-11-18 06:43:54 +0530

    Updated Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,11 @@
+import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import Services from './components/Services';
+import Books from "./components/Books";
+import Courses from "./components/Courses";
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import Navigation from './components/Navigation';
-import Books from "./components/Books";
-import Courses from "./components/Courses";
 
 export default function Home() {
   return (
