80eff289a624901b6a70a1e3ef4bb70e79f36007
Syed Saleha <117304081+Syedsaleha@users.noreply.github.com>
2025-11-19 07:17:47 +0530

    Updated Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,12 +9,7 @@ export default function Footer() {
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-12 mb-12">
 
-          {/* BRAND + DESCRIPTION */}
-          <div className="col-span-1 md:col-span-2">
-            <div className="flex items-center space-x-2">
-              <Cloud size={32} className="text-yellow-400" />
-
-                  {/* Logo */}
+              {/* Logo */}
 <div className="flex items-center">
   <img
     src="/your-logo.png"
