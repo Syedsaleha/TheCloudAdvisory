42ef85c635eaf5ecac0fac324b47af339367b1e3
Syed Saleha <117304081+Syedsaleha@users.noreply.github.com>
2025-11-19 07:05:41 +0530

    Updated Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -30,7 +30,7 @@ export default function Navigation() {
   <img
     src="/your-logo.png"
     alt="Logo"
-    className="h-15 w-auto object-contain"
+    className="h-14 w-auto object-contain"
   />
 </div>
 
